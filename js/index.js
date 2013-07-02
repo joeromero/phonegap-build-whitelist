795cd3858ef19d9240dcff36335300b55c69cb06
Ryan Willoughby <ryan.willoughby@adobe.com>
2013-07-02 14:08:26 -0700

    copied and tweaked from phonegap-start

---
+++ js/index.js
@@ -0,0 +1,84 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+
+        app.testDomains();
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    },
+
+    testDomains: function() {
+        for (var i in app.domains) {
+            $.ajax({
+                url:app.domains[i].url,
+                success:function() {
+                    if (app.domains[i].should_work) {
+                        console.log(app.domains[i].url + " reachable as exepcted");
+                    } else {
+                        console.log(app.domains[i].url + " should not be reachable");
+                    }
+                },
+                error:function() {
+                    if (app.domains[i].should_work) {
+                        console.log(app.domains[i].url + " should be reachable");
+                    } else {
+                        console.log(app.domains[i].url + " unreachable as exepcted");
+                    } 
+                }
+            })
+        }
+    },
+
+    domains:[
+        {
+            url:"http://phonegap.com",
+            should_work: true
+        },
+        {
+            url:"http://build.phonegap.com",
+            should_work: false
+        }
+    ]
+};
